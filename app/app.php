4835dbd95055dcd9164d5e7062377553499bc8de
Ashlin Aronin & Ike Mattice <student@epicodus.com>
2015-08-04 11:52:13 -0700

    Add CD display with pictures

--- app/app.php
+++ app/app.php
@@ -1,10 +1,42 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Cd.php";
 
     $app = new Silex\Application();
 
     $app->get("/", function() {
-        return "Home";
+        $first_cd = new CD("Master of Reality", "Black Sabbath", "images/reality.jpg", 10.99);
+        $second_cd = new CD ("Electric Ladyland", "Jimi Hendrix", "images/ladyland.jpg", 10.99);
+        $third_cd = new CD ("Nevermind", "Nirvana", "images/nevermind.jpg", 10.99);
+        $fourth_cd = new CD ("I don't get it", "Pork Lion", "images/porklion.png", 49.99);
+        $cds = array($first_cd, $second_cd, $third_cd, $fourth_cd);
+
+        $output = "
+        <!DOCTYPE html>
+        <html>
+            <head>
+            <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css'>
+                <title>CD World</title>
+            </head>
+            <body>
+        ";
+        foreach ($cds as $album){
+            $output = $output . "
+                <div class='row'>
+                    <div class='col-md-6'>
+                        <img src=" . $album->getCoverArt() . ">
+                    </div>
+                    <div class='col-md-6'>
+                        <p>" . $album->getTitle() . " </p>
+                        <p>By " . $album->getArtist() . "</p>
+                        <p>$" . $album->getPrice() . " </p>
+                    </div>
+                </div>
+                ";
+        }
+
+        $output = $output . "</body></html>";
+        return $output;
     });
 
     return $app;
